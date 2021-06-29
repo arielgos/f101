2598afbbc3b33aa47b8fbc8a9f1c5030b3b7d2f8
Ariel Ortuño Solis <arielg.os@gmail.com>
2021-06-28 21:55:19 -0400

    Adding Files

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAeT-TyrKZ8tlaWT4aBOCn744zoOcPrF8U",
+    authDomain: "f101-80e53.firebaseapp.com",
+    projectId: "f101-80e53",
+    storageBucket: "f101-80e53.appspot.com",
+    messagingSenderId: "777561954223",
+    appId: "1:777561954223:web:56aaf8fe66a1b8b770415a",
+    measurementId: "G-0D6H2J3Y9B"
+  };
+firebase.initializeApp(firebaseConfig);
+
+
+$(document).ready(function(){
+    console.log("App Start");
+});
