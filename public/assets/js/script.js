02f7354c21166024720fc43ee77e1785e342db87
Ariel Ortuño Solis <arielg.os@gmail.com>
2021-06-29 21:44:41 -0400

    Codigo actualizado

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,5 @@
+var firebaseUser = null;
+
 const firebaseConfig = {
   apiKey: "AIzaSyAeT-TyrKZ8tlaWT4aBOCn744zoOcPrF8U",
   authDomain: "f101-80e53.firebaseapp.com",
@@ -13,23 +15,46 @@ var provider = new firebase.auth.GoogleAuthProvider();
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    console.log(user.displayName);
+    firebaseUser = user;
+  } else {
+    firebaseUser = null;
   }
+  loadInterface();
 });
 
 $(document).ready(function () {
   console.log("App Start");
-  $("html").click(function () {
-    console.log("Body click");
+  //inicio de sesion
+  $(".menu .login").click(function () {
     firebase.auth()
       .signInWithPopup(provider)
       .then((result) => {
-        var credential = result.credential;
-        var token = credential.accessToken;
-        var user = result.user;
-        console.log(user.displayName);
+        firebaseUser = result.user;
+        
       }).catch((error) => {
         console.error(error);
       });
   });
+
+  $(".menu .logout").click(function () {
+    firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      console.error(error);
+    });
+  });
+
 });
+
+function loadInterface() {
+  if (firebaseUser != null) {
+    $(".menu .name img").attr({ src: firebaseUser.photoURL });
+    $(".menu .name span").html(firebaseUser.displayName);
+    $(".menu .logout").show();
+    $(".menu .login").hide();
+  } else {
+    $(".menu .name").html("");
+    $(".menu .logout").hide();
+    $(".menu .login").show();
+  }
+}
