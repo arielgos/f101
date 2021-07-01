fd2d9475d9d937dda0c897ff69f31ffcfb41ab92
Ariel Ortuño Solis <arielg.os@gmail.com>
2021-06-30 21:20:59 -0400

    Adding files

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,10 +9,14 @@ const firebaseConfig = {
   appId: "1:777561954223:web:56aaf8fe66a1b8b770415a",
   measurementId: "G-0D6H2J3Y9B"
 };
-firebase.initializeApp(firebaseConfig);
 
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var firestore = firebase.firestore();
 var provider = new firebase.auth.GoogleAuthProvider();
 
+//listener de la sesion
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     firebaseUser = user;
@@ -30,11 +34,23 @@ $(document).ready(function () {
       .signInWithPopup(provider)
       .then((result) => {
         firebaseUser = result.user;
+        firestore.collection("users")
+          .doc(firebaseUser.uid)
+          .set({
+            id: firebaseUser.uid,
+            user: firebaseUser.displayName,
+            image: firebaseUser.photoURL,
+          }).then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+          }).catch(function (error) {
+            //console.error("Error adding document: ", error);
+          });
       }).catch((error) => {
         console.error(error);
       });
   });
 
+  //cerrar sesion
   $(".menu .logout").click(function () {
     firebase.auth().signOut().then(() => {
       firebaseUser = null;
@@ -42,19 +58,56 @@ $(document).ready(function () {
       console.error(error);
     });
   });
+
+  //listener de usuarios
+  firestore.collection("users")
+    .onSnapshot(function (querySnapshot) {
+      querySnapshot.docChanges().forEach(function (change) {
+        if (change.type === "added") {
+          createUser(change.doc.data());
+        }
+      });
+    });
+
+  $(".loading").toggle();
+
 });
 
 function loadInterface() {
   console.log("loading interface...");
   if (firebaseUser !== null) {
+    console.log(firebaseUser.uid);
     $(".menu .name img").attr({ src: firebaseUser.photoURL });
     $(".menu .name span").html(firebaseUser.displayName);
     $(".menu .logout").show();
     $(".menu .login").hide();
+    $(".chat").show();
+    $(".users").css("width", "70%");
   } else {
     $(".menu .name img").attr({ src: "" });
     $(".menu .name span").html("");
     $(".menu .logout").hide();
     $(".menu .login").show();
+    $(".chat").hide();
+    $(".users").css("width", "100%");
   }
+}
+
+function createUser(data) {
+  var div = $("<div>").attr({ class: "user" });
+  div.append("<img src='" + data.image + "'>");
+  div.append("<span>" + data.user + "</span>");
+  $(".users").append(div);
+  animateUser(div);
+}
+
+function animateUser(obj) {
+  var top = Math.floor(Math.random() * $(".users").height());
+  var left = Math.floor(Math.random() * $(".users").width() - 100);
+  obj.animate({
+    top: top + "px",
+    left: left + "px"
+  }, 1500, function () {
+    animateUser(obj)
+  });
 }
